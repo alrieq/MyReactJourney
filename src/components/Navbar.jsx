bd06302dd743f1eebc5bcc4c9963f622ace33810
alrieq <alrieq@live.com>
2023-03-29 22:36:56 +0300

    MyReactJourney

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Navbar(){
+    return (
+      <div>
+        <nav>
+            <h1>Welcome to My Website</h1>
+            <ul>
+                <li>Home</li>
+                <li>Menu</li>
+                <li>Contact Us</li>
+            </ul>
+            <a href="https://kau.edu.sa/" target="_blanc">KAU</a>
+            <p>This is a wesite for king abdulaziz university</p>
+        </nav>
+    </div>  
+    )
+  }
+
+  export default Navbar;
